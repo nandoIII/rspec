fa8ca661eee56a68f69a2a8949a8b4995ab56f39
Hernando <nando_pdiaz@hotmail.com>
2014-03-07 22:26:19 -0500

    Example rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zomapp::Application.config.secret_token = 'fc5b0fb0c8e9334ce8b3565b43b20a1baefcc033673c02324b483535dd70ab72ba1188e7f07c7406dd8a4e373788d60c0dcc195e041d2ac85562979e7ba3c6f1'
